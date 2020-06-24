7f5dbe9dbf24f91ae5a29915c1afa209341807a0
zmx2321 <zmx2321@163.com>
2020-06-24 13:41:28 +0800

    init

--- algorithm/other/algorithm-base/arr_sort/js/index.js
+++ algorithm/other/algorithm-base/arr_sort/js/index.js
@@ -1,14 +1,8 @@
+let array = [9, 4, 3, 2, 1, 8, 3, 6, 7, 0, 1];
+
 function sortNumber(a, b){
 	return a - b
 }
 
-var arr = new Array(6)
-arr[0] = "10"
-arr[1] = "5"
-arr[2] = "40"
-arr[3] = "25"
-arr[4] = "1000"
-arr[5] = "1"
-
-console.log(arr);
-console.log(arr.sort(sortNumber));
+console.log(array);
+console.log(array.sort(sortNumber));
