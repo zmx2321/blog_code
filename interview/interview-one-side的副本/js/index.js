849e0e1d944ac24c7c8c3ed651c0f06432c01b4b
zmx2321 <zmx2321@163.com>
2021-07-22 18:29:00 +0800

    deploy

---
+++ interview/interview-one-side的副本/js/index.js
@@ -0,0 +1,21 @@
+// test1
+let test1 = ()=> {
+    return new Promise((resolve, reject) => {
+        // 返回写函数里面你要执行的内容
+        resolve(
+            console.log("test1, 1111")
+        );
+    })
+}
+
+// test2
+let test2 = () => {
+    setTimeout(function () {
+        console.log("test2, 2222");
+    }, 2000)
+}
+
+// 先执行test1，再执行test2
+test1().then(() => {
+    test2();
+}) 
