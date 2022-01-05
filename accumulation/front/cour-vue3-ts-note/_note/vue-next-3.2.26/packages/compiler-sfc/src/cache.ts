0631a8f0e32fdf625e35848364f058a5e3fe15a2
zmx2321 <zmx2321@163.com>
2022-01-05 09:44:39 +0800

    deploy

--- accumulation/front/cour-vue3-ts-note/_note/vue-next-3.2.26/packages/compiler-sfc/src/cache.ts
+++
@@ -1,7 +0,0 @@
-import LRU from 'lru-cache'
-
-export function createCache<T>(size = 500) {
-  return __GLOBAL__ || __ESM_BROWSER__
-    ? new Map<string, T>()
-    : (new LRU(size) as any as Map<string, T>)
-}
