e3436c8b39ed5e2e949b8248733c97ffc35d592a
zmx2321 <zmx2321@163.com>
2023-02-08 16:04:39 +0800

    deploy

--- accumulation/front/cour-vue3-ts-note/webpack_vue3_demo/src/main.js
+++ accumulation/front/cour-vue3-ts-note/webpack_vue3_demo/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 // import App from './App.vue'
 import router from './router'
 
-import App from './views/01_vue3Test'
+// import App from './views/01_父组件传递子组件'
+import App from './views/02_子组件传递父组件'
 
 createApp(App).use(router).mount('#app')
---
+++ accumulation/front/cour-vue3-ts-note/webpack_vue3_demo/src/views/01_父组件传递子组件/ShowMessage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2 v-bind="$attrs">{{title}}</h2>
+    <p>{{content}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    // props: ['title', 'content']
+    inheritAttrs: false,
+    props: {
+      title: String,
+      content: {
+        type: String,
+        required: true,
+        default: "123"
+      },
+      counter: {
+        type: Number
+      },
+      // 在父传子的时候,如果props里面是对象,并且需要给他默认值的时候,需要写成方法,
+      // 因为使用组件的时候会多次使用,即会创建多个组件实例,每个组件实例中都有一个props属性,props里面的默认对象也是一样的,他们都指向的是一个对象,如果一个组件改了值,那么所有的使用了那个组件的对象的值都会发生改变
+      info: {
+        type: Object,
+        default() {
+          return {name: "why"}
+        }
+      },
+      messageInfo: {
+        type: String
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
