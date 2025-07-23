265dce320922883fa81722f5afa7e3be27f3efc3
zmx2321 <zmx2321@163.com>
2025-07-23 14:49:41 +0800

    deploy

--- accumulation/front/react-course/react-course-module1/base-demo/src/App.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Example1 from './example1/index'
+import Example2 from './example2/index'
+
+const name = "first app"
 
 function App () {
     return (
         <div className="App">
-            app
+            app + { name }
+            <Example1 />
+            <Example2 />
         </div>
     );
 }
--- accumulation/front/react-course/react-course-module1/base-demo/src/index.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/index.js
@@ -10,7 +10,11 @@ import App from './App';
 // ReactDom 有一个render 方法
 // 创建react虚拟根节点 - 获取public下的 <div id="root"></div>
 const root = ReactDOM.createRoot(document.querySelector('#root'));
+// render里面的内容表示 jsx, 表示js+xml
+// jsx是一个对象,是虚拟 dom 语法糖
+// 将render里面的虚拟dom,即jsx,直接插入到 root 根节点
 root.render(
+    // jsx 对象
     <React.StrictMode>
         <App />
     </React.StrictMode>
