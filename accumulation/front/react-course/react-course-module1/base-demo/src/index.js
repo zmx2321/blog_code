5371d131b2dedfe9d0ab0373815d4e58d44a3ac5
zmx2321 <zmx2321@163.com>
2025-07-23 13:39:38 +0800

    deploy

--- accumulation/front/react-course/react-course-module1/base-demo/src/index.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/index.js
@@ -1,17 +1,17 @@
+/**
+ * 项目的入口文件
+ * 必须先导入react和react-dom包
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDom 有一个render 方法
+// 创建react虚拟根节点 - 获取public下的 <div id="root"></div>
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
