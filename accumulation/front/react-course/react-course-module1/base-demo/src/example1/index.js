8ab3f27c3307eafc457f1c2e8ba2d4613f85b291
zmx2321 <zmx2321@163.com>
2025-07-23 14:59:30 +0800

    deploy

--- accumulation/front/react-course/react-course-module1/base-demo/src/example1/index.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/example1/index.js
@@ -20,33 +20,3 @@ function Example1 () {
 }
 
 export default Example1;
-
-
-
-// // import { useState, useEffect } from 'react';
-// import './App.css';
-// import example1 from './example1/index.js'
-
-// const name = "first app"
-
-// // const [time, setTime] = useState(new Date().toLocaleTimeString())
-
-// /* useEffect(() => {
-//     const intervalId = setInterval(() => {
-//         setTime(new Date().toLocaleTimeString());
-//     }, 1000);
-
-//     // 清除定时器，避免内存泄漏
-//     return () => clearInterval(intervalId);
-// }, []); // 空依赖数组表示这个effect只在组件挂载和卸载时运行一次 */
-
-// function App () {
-//     return (
-//         <div className="App">
-//             app + { name }
-//             <example1 />
-//         </div>
-//     );
-// }
-
-// export default App;
