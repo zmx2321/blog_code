f3a299767fb4bccce8de3d3350771140ebe3de4b
zmx2321 <zmx2321@163.com>
2025-07-24 16:33:32 +0800

    deploy

--- accumulation/front/react-course/react-course-module1/base-demo/src/example10/index.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/example10/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
+// 在父组件中使用
+import EnhancedComponent from './components/HOC';
+
+/**
+ * 高阶组件不是组件,本质上是一个函数
+ * 接收一个组件或者多个组件,返回一个新组件
+ * 这个组件就是高阶组件
+ */
+
 const Example10 = () => {
     return (
         <>
-            <h2>
-                111
-            </h2>
+            {/* // 渲染时会自动输出日志：
+            // 组件 BaseComponent 已挂载
+            // 组件 BaseComponent 已更新 */}
+            <EnhancedComponent title="示例组件" />
         </>
-    )
-}
+    );
+};
 
 export default Example10;
