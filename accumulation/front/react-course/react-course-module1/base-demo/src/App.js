43f9acb9ecfbfd950036919bc81b37fe553efeb3
zmx2321 <zmx2321@163.com>
2025-07-24 14:22:56 +0800

    deploy

--- accumulation/front/react-course/react-course-module1/base-demo/src/App.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/App.js
@@ -9,6 +9,28 @@ import Example5 from './example5/index'
 import Example6 from './example6/index'
 import Example7 from './example7/index'
 import Example8 from './example8/index'
+import Example9 from './example9/index'
+import Example10 from './example10/index'
+import Example11 from './example11/index'
+import Example12 from './example12/index'
+import Example13 from './example13/index'
+import Example14 from './example14/index'
+import Example15 from './example15/index'
+import Example16 from './example16/index'
+import Example17 from './example17/index'
+import Example18 from './example18/index'
+import Example19 from './example19/index'
+import Example20 from './example20/index'
+import Example21 from './example21/index'
+import Example22 from './example22/index'
+import Example23 from './example23/index'
+import Example24 from './example24/index'
+import Example25 from './example25/index'
+import Example26 from './example26/index'
+import Example27 from './example27/index'
+import Example28 from './example28/index'
+import Example29 from './example29/index'
+import Example30 from './example30/index'
 
 const name = "first app"
 
@@ -17,9 +39,97 @@ const exam3Name = "example3Data"
 function App () {
     return (
         <div className="App">
+            {/* example30:
+            <Example30 />
+            <br />
+
+            example29:
+            <Example29 />
+            <br />
+
+            example28:
+            <Example28 />
+            <br />
+
+            example27:
+            <Example27 />
+            <br />
+
+            example26:
+            <Example26 />
+            <br />
+
+            example25:
+            <Example25 />
+            <br />
+
+            example24:
+            <Example24 />
+            <br />
+
+            example23:
+            <Example23 />
+            <br />
+
+            example22:
+            <Example22 />
+            <br />
+
+            example21:
+            <Example21 />
+            <br />
+
+            example20:
+            <Example20 />
+            <br />
+
+            example19:
+            <Example19 />
+            <br />
+
+            example18:
+            <Example18 />
+            <br />
+
+            example17:
+            <Example17 />
+            <br />
+
+            example16:
+            <Example16 />
+            <br />
+
+            example15:
+            <Example15 />
+            <br />
+
+            example14:
+            <Example14 />
+            <br />
+
+            example13:
+            <Example13 />
+            <br />
+
+            example12:
+            <Example12 />
+            <br />
+
+            example11:
+            <Example11 />
+            <br />
+
+            example10:
+            <Example10 />
+            <br />
+
+            example9:
+            <Example9 />
+            <br />
+
             example8:
             <Example8 />
-            <br />
+            <br /> */}
 
             example7:
             <Example7 />
