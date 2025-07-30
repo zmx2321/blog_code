f2b1aa8d04b0349817a294591e32ca89c105859f
zmx2321 <zmx2321@163.com>
2025-07-30 13:35:12 +0800

    deploy

--- accumulation/front/react-course/react-course-module1/base-demo/src/example22/index.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/example22/index.js
@@ -4,13 +4,13 @@ import { store } from './utils/store';
 import CounterView from './utils/features/counter/CounterView';
 
 // 定义一个名为Example21的函数组件
-const Example23 = () => (
+const Example22 = () => (
     <>
-        <h2>Example23 connect</h2>
+        <h2>Example22 connect</h2>
         <Provider store={store}>
             <CounterView />
         </Provider>
     </>
 );
 
-export default Example23;
+export default Example22;
--- accumulation/front/react-course/react-course-module1/base-demo/src/example22/utils/features/counter/CounterView.js
+++ accumulation/front/react-course/react-course-module1/base-demo/src/example22/utils/features/counter/CounterView.js
@@ -1,21 +1,42 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+// 导入React核心库
+import React from 'react';
+// 从react-redux导入connect高阶组件
 import { connect } from 'react-redux';
+// 导入counterSlice中定义的action creators
+import { increment, decrement } from './counterSlice';
 
-const CounterView = () => {
-    const count = useSelector(state => state.counter.value);
-    const dispatch = useDispatch();
-
+// 定义CounterView组件（改为接收props的普通函数组件）
+const CounterView = ({
+    count,      // 通过connect注入的state
+    increment,  // 通过connect注入的action
+    decrement   // 通过connect注入的action
+}) => {
     return (
         <>
             <h2>
                 redux toolkit 示例 <br />
                 当前计数: {count}
             </h2>
-            <button onClick={() => dispatch(increment())}>+</button>
-            <button onClick={() => dispatch(decrement())}>-</button>
+            {/* 直接使用props中的action方法 */}
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
         </>
     );
 }
 
-export default connect(state => state.counter)(CounterView);
+// mapStateToProps：将Redux state映射到组件props
+const mapStateToProps = (state) => ({
+    count: state.counter.value  // 从counter切片获取value值
+});
+
+// mapDispatchToProps：将action creators绑定到组件props
+const mapDispatchToProps = (dispatch) => ({
+    increment: () => dispatch(increment()),  // 显式包装避免事件对象传递
+    decrement: () => dispatch(decrement())
+});
+
+// 使用connect高阶组件包裹组件：
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps  // 使用修正后的dispatch映射
+)(CounterView);
